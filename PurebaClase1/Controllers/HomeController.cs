4f84da4894346f1c0e3bb78627330949b9d3c4d6
Mike <aleja_1456_6@hotmail.com>
2023-05-23 21:55:01 -0600

    Se agrega pantalla acerca de nosotros junto su css e imagen

--- PurebaClase1/Controllers/HomeController.cs
+++ PurebaClase1/Controllers/HomeController.cs
@@ -23,6 +23,12 @@ namespace PurebaClase1.Controllers
             return View();
         }
 
+        public IActionResult Acercadenosotros()
+        {
+            return View();
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
