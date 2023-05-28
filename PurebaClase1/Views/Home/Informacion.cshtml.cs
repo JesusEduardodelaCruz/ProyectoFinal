d21d2c76ec44a5645b051c0567a06f48d9e5fa17
JesusEduardodelaCruz <Jesus.delacruzdlrs@uanl.edu-mx>
2023-05-28 13:53:36 -0600

    Se creo la pestaña de informacion pero aun le faltan arreglos para que se pudiera ver

---
+++ PurebaClase1/Views/Home/Informacion.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PurebaClase1.Views.Home
+{
+    public class InformacionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
