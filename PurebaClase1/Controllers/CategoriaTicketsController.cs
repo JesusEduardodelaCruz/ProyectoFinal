198b733afe21c94bfed6990a839d691d583c09f7
Mike <aleja_1456_6@hotmail.com>
2023-05-29 21:15:35 -0600

    Se crea CRUD para tickets y categori ticket

---
+++ PurebaClase1/Controllers/CategoriaTicketsController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PurebaClase1.Models.dbModels;
+
+namespace PurebaClase1.Controllers
+{
+    public class CategoriaTicketsController : Controller
+    {
+        private readonly ProyectoBDContext _context;
+
+        public CategoriaTicketsController(ProyectoBDContext context)
+        {
+            _context = context;
+        }
+
+        // GET: CategoriaTickets
+        public async Task<IActionResult> Index()
+        {
+              return _context.CategoriaTickets != null ? 
+                          View(await _context.CategoriaTickets.ToListAsync()) :
+                          Problem("Entity set 'ProyectoBDContext.CategoriaTickets'  is null.");
+        }
+
+        // GET: CategoriaTickets/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.CategoriaTickets == null)
+            {
+                return NotFound();
+            }
+
+            var categoriaTicket = await _context.CategoriaTickets
+                .FirstOrDefaultAsync(m => m.IdCategoria == id);
+            if (categoriaTicket == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoriaTicket);
+        }
+
+        // GET: CategoriaTickets/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CategoriaTickets/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdCategoria,Descripcion")] CategoriaTicket categoriaTicket)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(categoriaTicket);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoriaTicket);
+        }
+
+        // GET: CategoriaTickets/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.CategoriaTickets == null)
+            {
+                return NotFound();
+            }
+
+            var categoriaTicket = await _context.CategoriaTickets.FindAsync(id);
+            if (categoriaTicket == null)
+            {
+                return NotFound();
+            }
+            return View(categoriaTicket);
+        }
+
+        // POST: CategoriaTickets/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdCategoria,Descripcion")] CategoriaTicket categoriaTicket)
+        {
+            if (id != categoriaTicket.IdCategoria)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(categoriaTicket);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CategoriaTicketExists(categoriaTicket.IdCategoria))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoriaTicket);
+        }
+
+        // GET: CategoriaTickets/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.CategoriaTickets == null)
+            {
+                return NotFound();
+            }
+
+            var categoriaTicket = await _context.CategoriaTickets
+                .FirstOrDefaultAsync(m => m.IdCategoria == id);
+            if (categoriaTicket == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoriaTicket);
+        }
+
+        // POST: CategoriaTickets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.CategoriaTickets == null)
+            {
+                return Problem("Entity set 'ProyectoBDContext.CategoriaTickets'  is null.");
+            }
+            var categoriaTicket = await _context.CategoriaTickets.FindAsync(id);
+            if (categoriaTicket != null)
+            {
+                _context.CategoriaTickets.Remove(categoriaTicket);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CategoriaTicketExists(int id)
+        {
+          return (_context.CategoriaTickets?.Any(e => e.IdCategoria == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ PurebaClase1/Controllers/TicketsController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PurebaClase1.Models.dbModels;
+
+namespace PurebaClase1.Controllers
+{
+    public class TicketsController : Controller
+    {
+        private readonly ProyectoBDContext _context;
+
+        public TicketsController(ProyectoBDContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Tickets
+        public async Task<IActionResult> Index()
+        {
+            var proyectoBDContext = _context.Tickets.Include(t => t.IdCategoriaNavigation).Include(t => t.IdUsuarioNavigation);
+            return View(await proyectoBDContext.ToListAsync());
+        }
+
+        // GET: Tickets/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Tickets == null)
+            {
+                return NotFound();
+            }
+
+            var ticket = await _context.Tickets
+                .Include(t => t.IdCategoriaNavigation)
+                .Include(t => t.IdUsuarioNavigation)
+                .FirstOrDefaultAsync(m => m.IdTicket == id);
+            if (ticket == null)
+            {
+                return NotFound();
+            }
+
+            return View(ticket);
+        }
+
+        // GET: Tickets/Create
+        public IActionResult Create()
+        {
+            ViewData["IdCategoria"] = new SelectList(_context.CategoriaTickets, "IdCategoria", "IdCategoria");
+            ViewData["IdUsuario"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Tickets/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdTicket,Descripcion,IdUsuario,IdCategoria")] Ticket ticket)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(ticket);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdCategoria"] = new SelectList(_context.CategoriaTickets, "IdCategoria", "IdCategoria", ticket.IdCategoria);
+            ViewData["IdUsuario"] = new SelectList(_context.Users, "Id", "Id", ticket.IdUsuario);
+            return View(ticket);
+        }
+
+        // GET: Tickets/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Tickets == null)
+            {
+                return NotFound();
+            }
+
+            var ticket = await _context.Tickets.FindAsync(id);
+            if (ticket == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdCategoria"] = new SelectList(_context.CategoriaTickets, "IdCategoria", "IdCategoria", ticket.IdCategoria);
+            ViewData["IdUsuario"] = new SelectList(_context.Users, "Id", "Id", ticket.IdUsuario);
+            return View(ticket);
+        }
+
+        // POST: Tickets/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdTicket,Descripcion,IdUsuario,IdCategoria")] Ticket ticket)
+        {
+            if (id != ticket.IdTicket)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(ticket);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TicketExists(ticket.IdTicket))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdCategoria"] = new SelectList(_context.CategoriaTickets, "IdCategoria", "IdCategoria", ticket.IdCategoria);
+            ViewData["IdUsuario"] = new SelectList(_context.Users, "Id", "Id", ticket.IdUsuario);
+            return View(ticket);
+        }
+
+        // GET: Tickets/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Tickets == null)
+            {
+                return NotFound();
+            }
+
+            var ticket = await _context.Tickets
+                .Include(t => t.IdCategoriaNavigation)
+                .Include(t => t.IdUsuarioNavigation)
+                .FirstOrDefaultAsync(m => m.IdTicket == id);
+            if (ticket == null)
+            {
+                return NotFound();
+            }
+
+            return View(ticket);
+        }
+
+        // POST: Tickets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Tickets == null)
+            {
+                return Problem("Entity set 'ProyectoBDContext.Tickets'  is null.");
+            }
+            var ticket = await _context.Tickets.FindAsync(id);
+            if (ticket != null)
+            {
+                _context.Tickets.Remove(ticket);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TicketExists(int id)
+        {
+          return (_context.Tickets?.Any(e => e.IdTicket == id)).GetValueOrDefault();
+        }
+    }
+}
