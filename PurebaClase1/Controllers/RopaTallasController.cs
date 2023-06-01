85f61264886f4c5a10058b8e35bf9386e2d03b05
JesusEduardodelaCruz <Jesus.delacruzdlrs@uanl.edu-mx>
2023-05-31 22:52:14 -0600

    Se agrego la autorizacion a la pagina

--- PurebaClase1/Controllers/RopaTallasController.cs
+++ PurebaClase1/Controllers/RopaTallasController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +12,7 @@ using PurebaClase1.Models.dbModels;
 
 namespace PurebaClase1.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class RopaTallasController : Controller
     {
         public IActionResult Ropadmin()
